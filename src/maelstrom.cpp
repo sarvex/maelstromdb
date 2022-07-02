4bfd64855a55ed474b01e82e1887b6965f714936
CheranMahalingam <cheranm@outlook.com>
2022-07-01 23:11:03 -0400

    Updated cli to create nodes and modify cluster configuration

--- src/maelstrom.cpp
+++ src/maelstrom.cpp
@@ -1,11 +1,67 @@
+#include <getopt.h>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "concensus_module.h"
 #include "global_ctx_manager.h"
+#include "leader_proxy.h"
 #include "logger.h"
 #include "raft_client.h"
 #include "raft_server.h"
+
+std::vector<std::string> comma_separated_string(std::string address_str) {
+  std::stringstream ss(address_str);
+  std::vector<std::string> peers;
+  while (ss.good()) {
+    std::string address;
+    getline(ss, address, ',');
+    peers.push_back(address);
+  }
+  return peers;
+}
+
+void initialize_node(std::string& address, std::vector<std::string>& peers, bool leader) {
+  Logger::Info("Initializing...");
+
+  raft::GlobalCtxManager ctx(address);
+  ctx.ConcensusInstance()->StateMachineInit(2);
+  if (leader) {
+    ctx.ConcensusInstance()->InitializeConfiguration(peers);
+  }
+
+  std::thread client_worker = std::thread(&raft::RaftClient::AsyncCompleteRPC, ctx.ClientInstance());
+
+  ctx.ServerInstance()->ServerInit();
+
+  if (client_worker.joinable()) {
+    client_worker.join();
+  }
+
+  Logger::Info("Node terminated");
+}
+
+void set_configuration(
+    std::vector<std::string>& old_addresses,
+    std::vector<std::string>& new_addresses) {
+  std::cout << "Attempting to modify cluster configuration...\n";
+
+  raft::LeaderProxy proxy(old_addresses);
+  auto cluster = proxy.GetClusterConfiguration();
+  std::cout << "Existing configuration: ";
+  for (auto server:cluster.servers()) {
+    std::cout << server.address() << " ";
+  }
+  std::cout << "\nNew configuration: ";
+  for (auto address:new_addresses) {
+    std::cout << address << " ";
+  }
+  std::cout << "\n";
+}
+
+void help() {
+
+}
 
 int main(int argc, char* argv[]) {
   std::string address(argv[1]);
@@ -17,21 +73,59 @@ int main(int argc, char* argv[]) {
   Logger::SetLevel(Logger::LogLevel::DEBUG);
   Logger::SetLogConsole();
 
-  Logger::Info("Initializing...");
+  static struct option long_options[] = {
+    {"create", required_argument, NULL, 'n'},
+    {"cluster", required_argument, NULL, 'c'},
+    {"reconfigure", required_argument, NULL, 'r'},
+    {"leader", no_argument, NULL, 'l'},
+    {"help", no_argument, NULL, 'h'}
+  };
 
-  raft::GlobalCtxManager ctx(address);
-  ctx.ConcensusInstance()->StateMachineInit(2);
-  ctx.ConcensusInstance()->InitializeConfiguration(peer_ids);
+  std::string new_address;
+  std::vector<std::string> cluster;
+  std::vector<std::string> reconfigure;
+  bool initialize_as_leader = false;
+  while (true) {
+    int c = getopt_long(argc, argv, "c:r:h", long_options, NULL);
 
-  std::thread client_worker = std::thread(&raft::RaftClient::AsyncCompleteRPC, ctx.ClientInstance());
+    if (c == -1) {
+      break;
+    }
 
-  ctx.ServerInstance()->ServerInit();
-
-  if (client_worker.joinable()) {
-    client_worker.join();
+    switch (c) {
+      case 'n':
+        new_address = optarg;
+        break;
+      case 'c':
+        cluster = comma_separated_string(optarg);
+        break;
+      case 'r':
+        reconfigure = comma_separated_string(optarg);
+        break;
+      case 'l':
+        initialize_as_leader = true;
+      case 'h':
+        help();
+        exit(0);
+      default:
+        help();
+        exit(1);
+    }
   }
 
-  Logger::Info("Node terminated");
+  if (optind < argc) {
+    help();
+    exit(1);
+  }
+
+  if (cluster.size() > 0 && reconfigure.size() > 0) {
+    set_configuration(cluster, reconfigure);
+  }
+
+  if (new_address.size() > 0) {
+    initialize_node(new_address, cluster, initialize_as_leader);
+  }
+
   return 0;
 }
 
